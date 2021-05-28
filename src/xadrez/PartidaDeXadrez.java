2e9ce0e88159ba9b4a56e5fdf37e25205fe8655c
dreine-dev <dreineantonio@hotmail.com>
2021-05-28 12:38:15 -0300

    Camada de xadrex e Imprimimos o tabuleiro

---
+++ src/xadrez/PartidaDeXadrez.java
@@ -0,0 +1,23 @@
+package xadrez;
+
+import tabuleirojogo.Tabuleiro;
+
+public class PartidaDeXadrez {
+
+	private Tabuleiro tabuleiro;
+
+	public PartidaDeXadrez() {
+		tabuleiro = new Tabuleiro(8, 8);
+	}
+
+	public PecaDeXadrez[][] getPecaDeXadrez() {
+		PecaDeXadrez[][] mat = new PecaDeXadrez[tabuleiro.getLinha()][tabuleiro.getColuna()];
+		for (int i = 0; i < tabuleiro.getLinha(); i++) {
+			for (int j = 0; j < tabuleiro.getColuna(); j++) {
+				mat[i][j] = (PecaDeXadrez)tabuleiro.peca(i, j);
+			}
+		}
+		return mat;
+	}
+
+}
